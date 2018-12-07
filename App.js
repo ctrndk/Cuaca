307e381a9f3c796a6d9c14a0ea030b90b90f707a
Catra Andika <ctrndk@gmail.com>
2018-12-08 04:33:08 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import { StyleSheet, Text, StatusBar, ActivityIndicator, KeyboardAvoidingView, Platform, TextInput,  ImageBackground, View, TouchableOpacity } from 'react-native';
+
+import { fetchLocationId, fetchWeather } from './utils/api';
+import getImageForWeather from './utils/getImageForWeather';
+
+import SearchInput from './components/SearchInput';
+
+export default class App extends React.Component {
+
+constructor(props){
+  super(props);
+
+  this.state = {
+    loading: false,
+    error: false,
+    location: '',
+    temperature: 0,
+    weather: '',
+  };
+}
+
+componentDidMount(){
+ this.handleUpdateLocation('Catra Andika');
+}
+
+
+handleUpdateLocation = async city => {
+  if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId,
+          );
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+
+      } catch (e) {
+        this.setState({
+        loading: false,
+        error: true,
+      });
+    }
+  });
+};
+
+render() {
+  const { loading, error, location, weather, temperature } = this.state;
+
+return (
+  <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <StatusBar barStyle="light-content" />
+    <ImageBackground source={getImageForWeather(weather)} style={styles.imageContainer}
+    imageStyle={styles.image}
+    >
+    <View style={styles.detailsContainer}>
+
+      <ActivityIndicator animating={loading} color="white" size="large" />
+      
+      {!loading && (
+        <View>
+          {error && (
+
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Keyword belum ada atau tidak dapat ditemukan :(
+            </Text>
+          )}
+
+          {!error && (
+            <View>
+              <Text style={[styles.largeText, styles.textStyle]}>
+                {location}
+              </Text>
+              <Text style={[styles.smallText, styles.textStyle]}>
+                {weather}
+              </Text>
+              <Text style={[styles.largeText, styles.textStyle]}>
+                {`${Math.round(temperature)}° Celcius`}
+              </Text>
+            </View>
+          )}
+
+          <SearchInput
+          placeholder="Cari Kota"
+          onSubmit={this.handleUpdateLocation}
+          />
+        </View>
+      )}  
+
+            
+              <Text style = {[styles.foo, styles.miniText]}> Made with ❤ Catra Andika </Text>
+
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView> 
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#34495E',
+
+  },
+  red: {
+    color: 'red',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ?  'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
+
+  },
+  miniText:{
+    fontSize: 12,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  mediumText: {
+    fontSize: 26,
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  TextInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 40,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  },
+  imageContainer: { 
+    flex: 1, 
+  }, 
+
+
+   foo:{
+      color: '#fff',
+      alignSelf: 'center',
+      marginTop: 100,
+   },
+
+  image: { 
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover', 
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20, 
+  },
+
+});
